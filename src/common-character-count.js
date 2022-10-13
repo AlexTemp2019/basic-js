d24fda253356ec1dd1fa98909d891eef620ad6ea
AlexTemp <kag.temp@gmail.com>
2022-10-13 17:48:55 +0300

    feat: add encodeLine

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,9 +11,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+  let couther;
+  let stack;
+  let result = 0;
+
+  if (s1.length <= s2.length) {
+    couther = s1;
+    stack = s2;
+  } else {
+    couther = s2;
+    stack = s1;
+  }  
+ 
+  for (let i = 0; i < couther.length; i++) {
+    let j = stack.indexOf(couther[i]);
+    if (j >= 0) {
+      result++;
+      stack = stack.replace(couther[i], "");
+    }
+  }
+  return result;
 }
 
 module.exports = {
--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,9 +11,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  const iterator = (n+"").split(""); 
+  let result = 0;
+  for(let i = 0; i<iterator.length; i++) {
+    const current = [...iterator];
+    current.splice(i, 1);
+    let newNumber = parseInt(current.join(""));
+    result = result > newNumber ? result : newNumber;
+  }
+  return result;
 }
 
 module.exports = {
